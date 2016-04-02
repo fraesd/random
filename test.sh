94b7d50ec718373e2f62b4b307cf2d1d0158005d
Patryk Jurkiewicz <patryk.jurkiewicz@o2.pl>
2016-04-02 16:32:28 +0200

    pushing new file test.sh into the repo

---
+++ test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo """
+	I have not used git
+	for faaaaar too long of a time
+
+
+	As such, after a year(freakin year since I USED IT)
+
+	I need to remind myself of how to use it ehhh....
+"""
